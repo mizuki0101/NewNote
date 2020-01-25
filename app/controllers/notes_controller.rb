67683d8e7c0e895baeaf7b80d8febc610d555885
mizuki0101 <gresky11@gmail.com>
2020-01-25 19:02:47 +0900

    count追加

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,7 +3,7 @@ class NotesController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @notes = Note.includes(:user).order("created_at DESC").page(params[:page]).per(10)
+    @notes = Note.includes(user: :comments).order("created_at DESC").page(params[:page]).per(10)
   end
 
   def new
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,8 +58,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  mail = ENV['program.test.note@gmail.com']
-  pass = ENV['gxinuhdtrhkexcle']
+  mail = ENV['']
+  pass = ENV['']
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
